77203790ef77404d328d76fed96d6b1afec6ba44
rj1110634028 <jo901025@gmail.com>
2022-03-21 11:33:16 +0800

    print data success

--- mqtt-sub.js
+++ mqtt-sub.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const conn = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234",
+  password: "root",
   database: "rpi",
   port: 3306,
 });
--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 const express = require("express");
-const app = express();
 const fs = require("fs");
-
+const app = express();
+var cors = require("cors");
+app.use(cors());
 const mysql = require("mysql");
 
 const conn = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234",
+  password: "root",
   database: "rpi",
   port: 3306,
 });
 
 conn.connect(console.log("connect success!"));
-
+const test = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let i=-1
 app.get("/", function (req, res) {
-  const sql = "SELECT * From rpi_data order by idrpi_data desc limit 5";
+  const sql = "SELECT * From rpi_data order by id desc limit 10";
   const data = conn.query(sql, function (error, results, fields) {
     if (error) throw error(console.log("Error"));
-    console.log(results);
+    console.log(test[i==19?i=0:++i]);
     res.json(results);
   });
 });
 
-const server = app.listen(8081, function () {
+const server = app.listen(8082, function () {
   const host = server.address().address;
   const port = server.address().port;
-
-  console.log("应用实例，访问地址为 http://%s:%s", host, port);
 });
+
